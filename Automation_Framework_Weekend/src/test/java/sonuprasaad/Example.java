483b64a7d57791bfb1b8170055863555f29e67ab
ADMIN <ADMIN@DESKTOP-63KLCIR>
2025-06-07 14:43:28 +0530

    Fork has been added

---
+++ Automation_Framework_Weekend/src/test/java/sonuprasaad/Example.java
@@ -0,0 +1,12 @@
+package sonuprasaad;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Example {
+	@Test
+	public void test()
+	{
+		Reporter.log("Fork has been added",true);
+	}
+}
